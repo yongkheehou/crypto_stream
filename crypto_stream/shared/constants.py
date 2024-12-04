28730fb5065efbbc40520694f77d32232e06a837
yongkheehou <yongkheehou@gmail.com>
2024-12-04 10:03:06 +0800

    Update lock files

--- crypto_stream/shared/constants.py
+++ crypto_stream/shared/constants.py
@@ -1,4 +1,5 @@
 LOGGER_URL = "http://logger-service:8000"
 FLINK_URL = "http://flink-service:8001"
 KAFKA_URL = "http://kafka-service:8002"
+KAFKA_BOOTSTRAP_SERVERS = "kafka-broker:9092"
 STREAMLIT_URL = "http://streamlit-service:8003"
